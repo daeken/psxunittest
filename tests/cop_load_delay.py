74488aba1c8d30d1b0e5e88c177de50209073087
Cody Brocious <cody.brocious@gmail.com>
2016-06-12 23:52:33 -0600

    Added test for COP load delays.

---
+++ tests/cop_load_delay.py
@@ -0,0 +1,26 @@
+from test import *
+
+name = 'Load delay for COP'
+load = 0x80100000
+
+GPR[2] = 0x80110000
+Mem32[0x80110000] = 0xDEADBEEF
+
+code = '''
+	lw $3, 0($2)
+	mfc2 $3, $25
+	beq $3, $0, .zero
+	nop
+	addi $1, $0, 1
+	j .end
+	nop
+
+.zero:
+	addi $1, $0, 2
+	j .end
+
+.end:
+'''
+
+GPR[3] == 0
+GPR[1] == 1
